46e7c92bf938c48636377d7f3bc1c17dcd5c8957
NikolaiCyber <nikolaicyberspace@gmail.com>
2024-04-15 20:35:38 +0300

    Create DB

---
+++ Server/Entities/MessageEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Entities
+{
+    public class MessageEntity : BaseEntity
+    {
+        public string Text { get; set; } = string.Empty;
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [ForeignKey("Chat")]
+        public int ChatId { get; set; }
+        public UserEntity? User { get; set; }
+        public ChatEntity? Chat { get; set; }
+    }
+}
