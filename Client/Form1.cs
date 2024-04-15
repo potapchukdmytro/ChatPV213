6c3beba0275840a8f10eacd1beff36c2e74ed2e2
martyniuk0208 <taniyha.m@gmail.com>
2024-04-15 21:01:44 +0300

    fix

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -27,7 +27,7 @@ namespace Client
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             SignIn signIn = new SignIn();
