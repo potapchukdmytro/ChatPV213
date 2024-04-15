c90e0b7cecc49e570411e4bf0c3f97ffc4cddf39
Dmytro Potapchuk <traiget@gmail.com>
2024-04-15 18:28:53 +0300

    Add project files.

---
+++ Server/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("workstation id=ChatPV213.mssql.somee.com;packet size=4096;user id=sweetdie_SQLLogin_2;pwd=q1fmmrgev5;data source=ChatPV213.mssql.somee.com;persist security info=False;initial catalog=ChatPV213;TrustServerCertificate=True");
+        }
+    }
+
+}
